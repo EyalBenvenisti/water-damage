bde275d713d8b8ba09137f0fe166259b3bad71f4
Kaizen <eyal@vsense.co.il>
2024-10-10 19:00:36 -0500

    Sitemap off

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://water-damage.vercel.app',  // Replace with your website URL
-  generateRobotsTxt: true,  // Generates robots.txt file
-  // Optional: Change the frequency of crawling and priority of certain pages
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 5000,
-  generateIndexSitemap: false,
+//  siteUrl: 'https://water-damage.vercel.app',  // Replace with your website URL
+//  generateRobotsTxt: true,  // Generates robots.txt file
+//  Optional: Change the frequency of crawling and priority of certain pages
+//  changefreq: 'daily',
+//  priority: 0.7,
+//  sitemapSize: 5000,
+//  generateIndexSitemap: false,
 }
