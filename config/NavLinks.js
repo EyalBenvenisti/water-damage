57aeb5852537dd86af54b57d41f7a4bbf0882a66
Kaizen <eyal@vsense.co.il>
2024-10-11 09:49:45 -0500

    config with lower N

--- config/NavLinks.js
+++ config/NavLinks.js
@@ -3,7 +3,7 @@ const navLinks = [
   { name: "Home", href: "/" },
   { name: "Services", href: "/services" },
   { name: "About", href: "/about" },
-  { name: "Blog", href: "/blog" },
+ //  { name: "Blog", href: "/blog" },
   { name: "Contact", href: "/contact" },
 ];
 
