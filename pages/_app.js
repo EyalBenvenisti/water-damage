d1d53cc70dc700c966259ceb08db1001ee3090a3
Kaizen <eyal@vsense.co.il>
2024-10-15 07:59:32 -0500

    rename navLinks

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/global.css'; 
+import '../styles/global.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Meta from '../components/Meta';
